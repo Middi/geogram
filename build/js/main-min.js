17d021481092e4694891b4602ec14d81acd56622
“Richard Middleton” <Richard@richardmiddleton.me>
2017-09-24 01:38:09 +0100

    Comments

--- build/js/main-min.js
+++ build/js/main-min.js
@@ -1,10 +1,13 @@
+// Config
+var token = '178595410.7e82061.56428f51fa2d4779856bf0af509aa91c';
+
+var url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}`;
+
+
+
 $(function() {
-
-    var token = '178595410.7e82061.56428f51fa2d4779856bf0af509aa91c';
-
-    var url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}`;
-
     
+    // Ajax Call
     function request() {
         $.ajax({
             type: 'GET',
@@ -16,12 +19,14 @@ $(function() {
         });
     }
 
+    // Build DOM, Called by request function
     function build(response) {
         response.data.forEach(function (item) {
             $(`<p>${item.likes.count}</p>`).appendTo($('.data'));
         });
     }
     
+    // Call request
     request();
 
 
