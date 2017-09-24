48c24a3f0e39d71270e59b3149fbdbb992b989a4
“Richard Middleton” <Richard@richardmiddleton.me>
2017-09-24 02:41:14 +0100

    clean js

--- build/js/main-min.js
+++ build/js/main-min.js
@@ -1,16 +1,11 @@
 // Config
 var token = '178595410.7e82061.56428f51fa2d4779856bf0af509aa91c';
-
 var url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}`;
-
 var geolocation;
-
-// Default location
 var defaultCity = 'Knottingley';
 
 
 $(function () {
-
     // Ajax Call
     function request(city) {
         $.ajax({
@@ -53,7 +48,9 @@ $(function () {
     request(defaultCity);
 
 
+    // ----------------
     // Get Input
+    // ----------------
 
     // --- On Enter Key --- //
     $("input").on("keydown", function search(e) {
